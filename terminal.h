1ff73d0840632b0b0fa705ae8ea617343a93df78
ilya.f <ilya.f@l4b-software.com>
2019-08-27 10:42:58 +0300

    little description

--- terminal.h
+++ terminal.h
@@ -70,7 +70,8 @@ private:
     QString const tmp_unrec ="Unrecognised command please enter <help>";
     QString const tmp_help =
            "clean - remove everything from terminal\n\
-debug - on|off debug mode";
+debug - on|off debug mode\n\
+repeat - on|off to repeat actions on debug map";
     QString tmp_debug_mode_on = "Set to debug mode";
     QString tmp_debug_mode_off = "Exit from debug mode";
     QString tmp_repeat_mode_on = "Repeat to debug mode on";
